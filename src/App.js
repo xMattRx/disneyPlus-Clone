73a4f68bff491a968b2cf3617729a6b22e4c8953
Matthew Rachid Romie Nascimento <matthewromie@hotmail.com>
2022-01-23 09:54:48 -0300

    Home ready

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
