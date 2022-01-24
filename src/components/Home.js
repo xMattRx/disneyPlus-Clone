562f2bb1ec85376507a60da5595d8faca84a6f9d
Matthew Rachid Romie Nascimento <matthewromie@hotmail.com>
2022-01-24 07:40:51 -0300

    Revert "Revert "Post movies""
    This reverts commit 95e25b16ae1c1c11a5b1d7bb34e732224ced8640.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,67 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import styled from "styled-components";
 import HomeBackground from '../assets/images/home-background.png';
+import { setMovies } from "../features/movie/movieSlice";
+import { selectUserName } from '../features/user/userSlice';
+import db from "../firebase";
 import ImgSlider from "./ImgSlider";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
+import Recommends from "./Recommend";
+import Trending from "./Trending";
 import Viewers from "./Viewers";
+
 const Home = (props) => {
+
+    const dispatch = useDispatch()
+    const userName = useSelector(selectUserName);
+    let recommends = [];
+    let newDisneys = [];
+    let originals = [];
+    let trending = [];
+
+    useEffect(() => {
+        db.collection('movies2').onSnapshot((snapshot) => {
+            snapshot.docs.map((doc) => {
+                console.log(recommends)
+                switch (doc.data().type) {
+
+                    case 'recommend':
+                        recommends = [...recommends, { id: doc.id, ...doc.data() }]
+                        break;
+                    case 'new':
+                        newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }]
+                        break;
+                    case 'original':
+                        originals = [...originals, { id: doc.id, ...doc.data() }]
+                        break;
+                    case 'trending':
+                        trending = [...trending, { id: doc.id, ...doc.data() }]
+                        break;
+
+                }
+            })
+            dispatch(
+                setMovies({
+                    recommend: recommends,
+                    newDisney: newDisneys,
+                    original: originals,
+                    trending: trending,
+                })
+            )
+        })
+    }, [userName])
+
+
     return (
         <Container>
             <ImgSlider />
             <Viewers />
+            <Recommends />
+            <NewDisney />
+            <Originals />
+            <Trending />
         </Container>
     )
 }
